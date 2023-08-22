507810fbad868a6c07662aa3ccdf18a2a0ee0633
fgabrielmorais <gabrielgamer.aleatoriow@gmail.com>
2023-08-22 15:44:00 -0300

    aprendendo react

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,36 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0); 
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://www.youtube.com/watch?v=AlnHNi0hdO0" target="_blank">
+          <img src="src/assets/miguelOhara.jfif" className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src="src/assets/milesMorales.jfif" className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Evento Canônico</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Quantidade de eventos canônicos: {count}
         </button>
-        <p>
+        {/* <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        </p> */}
+      </div>
+      
+      <div className="card">
+        <button onClick={() => setCount2((count2) => count2 + 1)}>
+          Quantidade de eventos canônicos: {count2}
+        </button>
+        {/* <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p> */}
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
